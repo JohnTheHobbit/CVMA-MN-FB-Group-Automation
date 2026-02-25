319d2faa98cd2aaec4899bc26ed9a0aa008833b9
JohnTheHobbit <john@boydclan.org>
2026-02-24 22:48:29 -0600

    Update link to Production

--- docs/js/config.js
+++ docs/js/config.js
@@ -4,7 +4,7 @@
 var CONFIG = {
     FB_APP_ID: '25699988686294647',
     FB_API_VERSION: 'v22.0',
-    N8N_WEBHOOK_URL: 'https://n8n.srv1047489.hstgr.cloud/webhook-test/fb-profile-collector',
+    N8N_WEBHOOK_URL: 'https://n8n.srv1047489.hstgr.cloud/webhook/fb-profile-collector',
     FB_GROUP_URL: 'https://www.facebook.com/groups/cvmamn',
     DEBUG: false
 };
